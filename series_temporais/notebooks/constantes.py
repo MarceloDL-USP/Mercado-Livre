92ede2f62debe8a8994d3c5e01db9fb6ea2d7026
Marcelo D <marcelo.delmondes.sigma@gmail.com>
2022-04-29 16:56:30 -0300

    3

---
+++ series_temporais/notebooks/constantes.py
@@ -0,0 +1,24 @@
+LAPSO_MEAN_1 = 15 # número de dias para se tomar uma média (mean) na reamostragem de dados para contrução do modelo de Séries Temporais
+SAZONALITY_MONTHS_1 = 6 # número de vezes em que se indentifou uma sazonalide
+TRAINNG_DATA_PERCENTAGE_1 = 0.8
+TEST_DATA_PERCENTAGE_1 = 0.1
+PLOT_ACF_LAGS_1 = 4
+PLOT_PACF_LAGS_1 = 4
+LAG_REDUCTION_FACTOR_1 = 1 # para lidar com seguinte problema ValueError: Length of endogenous variable must be larger the the number of lags used in the model and the number of observations burned in the log-likelihood calculation.
+
+
+LAPSO_MEAN_2 = 10 # número de dias para se tomar uma média (mean) na reamostragem de dados para contrução do modelo de Séries Temporais
+SAZONALITY_MONTHS_2 = 12 # número de vezes em que se indentifou uma sazonalide
+TRAINNG_DATA_PERCENTAGE_2 = 0.8
+TEST_DATA_PERCENTAGE_2 = 0.1
+PLOT_ACF_LAGS_2 = 4
+PLOT_PACF_LAGS_2 = 4
+LAG_REDUCTION_FACTOR_2 = 0.25 # para lidar com seguinte problema ValueError: Length of endogenous variable must be larger the the number of lags used in the model and the number of observations burned in the log-likelihood calculation.
+
+LAPSO_MEAN_3 = 10 # número de dias para se tomar uma média (mean) na reamostragem de dados para contrução do modelo de Séries Temporais
+SAZONALITY_MONTHS_3 = 12 # número de vezes em que se indentifou uma sazonalide
+TRAINNG_DATA_PERCENTAGE_3 = 0.8
+TEST_DATA_PERCENTAGE_3 = 0.1
+PLOT_ACF_LAGS_3 = 4
+PLOT_PACF_LAGS_3 = 4
+LAG_REDUCTION_FACTOR_3 = 1 # para lidar com seguinte problema ValueError: Length of endogenous variable must be larger the the number of lags used in the model and the number of observations burned in the log-likelihood calculation.
--- series_temporais/references/references.md
+++ series_temporais/references/references.md
@@ -5,3 +5,7 @@
 [[3] SARIMA Using Python – Forecast Seasonal Data](https://www.wisdomgeek.com/development/machine-learning/sarima-forecast-seasonal-data-using-python/)
 
 [[4] How to choose the parameters for the model](https://arauto.readthedocs.io/en/latest/how_to_choose_terms.html)
+
+[[5] Convert given Timestamp to Period](https://www.tutorialspoint.com/python-pandas-convert-given-timestamp-to-period-with-quarterly-frequency)
+
+[[6] Python Pandas – Convert PeriodIndex object to Timestamp and set the frequency](https://www.geeksforgeeks.org/python-pandas-convert-periodindex-object-to-timestamp-and-set-the-frequency/)
